05cb763fa8413275d5260e6c1d591f24bf706e8f
Michael Manke <michaelmanke00@gmail.com>
2021-03-16 02:27:21 +0100

    Design and Functionality
    I changed quite a bit. The design is pretty much ready now, the functionality is done. Removed the like button, changed the result display, added case of invalid stock

--- routes/api.js
+++ routes/api.js
@@ -4,40 +4,38 @@ module.exports = function (app) {
   const fetch = require('node-fetch')
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
+    .get(function (req, res) {
       let stocks = req.query.stock
-      let like = req.query.like
 
-      if(typeof stocks == 'string') {
+      if (typeof stocks == 'string') {
         stocks = stocks.toUpperCase()
-        if(like !== undefined) {
-          stocks += '&like=true'
-        }
-        fetch('https://stock-price-checker.freecodecamp.rocks/api/stock-prices?stock='+stocks)
-        .then(response => response.json())
-        .then(data => {
-          let responseObject = data
-          res.send(responseObject)
-        })
+        fetch('https://stock-price-checker.freecodecamp.rocks/api/stock-prices?stock=' + stocks)
+          .then(response => response.json())
+          .then(data => {
+            let responseObject = {
+              "stock": data.stockData.stock || 'No such stock exists',
+              "price": data.stockData.price
+            }
+            res.send(responseObject)
+          })
       } else {
-        for(let i = 0; i<stocks.length; i++) {
+        for (let i = 0; i < stocks.length; i++) {
           stocks[i] = stocks[i].toUpperCase()
         }
         let sol = stocks[0] + '&stock=' + stocks[1]
-        if(like !== undefined) {
-          sol += '&like=true'
-        }
-        fetch('https://stock-price-checker.freecodecamp.rocks/api/stock-prices?stock='+sol)
-        .then(response => response.json())
-        .then(data => {
-          let responseObject = data
-          res.send(responseObject)
-        })
+        fetch('https://stock-price-checker.freecodecamp.rocks/api/stock-prices?stock=' + sol)
+          .then(response => response.json())
+          .then(data => {
+            let responseObject = {
+              'stock1': data.stockData[0].stock || 'No such stock exists',
+              'price1': data.stockData[0].price,
+              'stock2': data.stockData[1].stock || 'No such stock exists',
+              'price2': data.stockData[1].price
+            }
+            res.send(responseObject)
+          })
 
       }
-
-      
-      
     });
-  
+
 };
